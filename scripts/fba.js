bb2a909981093bc60b02ef3bd8e92da064f57698
timetecinc <rayg@ttmemory.com>
2018-07-20 17:20:56 -0700

    fba updated2

--- scripts/fba.js
+++ scripts/fba.js
@@ -1,25 +1,44 @@
 //console.log("lines[0]");
 function getInfo (e){
 	
-  if ($('#dataTextArea').val() !="") {
+ if ($('#dataTextArea').val() !="") {
 
   
   var sku;
+  var singleSKU;
   var FNSKU;
+  var kitNum;
   var shippedQTY;
-  var item;
   var unitQTY;
   var id;
 
   
   var lines = $('#dataTextArea').val().split('\n');
-  for (var i = 0; i<lines.length -1; i++){
+
+  for (var i = 1; i<lines.length -1; i++){
     var line = lines[i].split('\t');//split tab
+    //get sku
     sku = line[0].split(".")[0];
+    //check Kit number and single sku
+    if(sku.indexOf('K')>-1){
+    	singleSKU = sku.split("K")[0];
+    	kitNum = sku.split("K")[1];
+    }else{
+    	singleSKU = sku;
+    	kitNum = 1;
+    }
+    //get fnsku
+    FNSKU = line[3];
 
-      console.log(sku);
-  }
+  	//get shipped qty
+  	shippedQTY = line[9];
+  	
+    console.log(sku);
+    console.log(kitNum);
+    console.log(FNSKU); 
+    console.log(shippedQTY);   	
+ }
 
-  }
+}
 return false;
 }
