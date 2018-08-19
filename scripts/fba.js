f92127061e19c68952bf7151f502c95b2eae623e
timetecinc <rayg@ttmemory.com>
2018-08-18 22:41:22 -0700

    add google auth

--- scripts/fba.js
+++ scripts/fba.js
@@ -1,4 +1,11 @@
 //console.log("lines[0]");
+var CLIENT_ID = '671839024565-i91in47kkr2aiu5evtujvgaj1dgs7g86.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyCaiqUn1UMOwuHVT01Mw8s-OSQl9QNHgpc';
+var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+
+      // Authorization scopes required by the API; multiple scopes can be
+      // included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
 
 function getInfo (){
  $( "#myTableBody" ).empty();
@@ -302,3 +309,64 @@ var ramData ='{"ram":['+
  '{"Timetec_Part": "30TT2535C-120G","ID": "SD03","TYPE": "SSD 120","Price": 22.5,"Weight": 0.2125},'+
  '{"Timetec_Part": "30TT2535C-240G","ID": "SD04","TYPE": "SSD 240","Price": 42,"Weight": 0.2125},'+
  '{"Timetec_Part": "30TT2535C-480G","ID": "SD05","TYPE": "SSD 480","Price": 81,"Weight": 0.2125}]}';
+
+
+function handleClientLoad() {
+        gapi.load('client:auth2', initClient);
+}
+function initClient() {
+        gapi.client.init({
+          apiKey: API_KEY,
+          clientId: CLIENT_ID,
+          discoveryDocs: DISCOVERY_DOCS,
+          scope: SCOPES
+        }).then(function () {
+          // Listen for sign-in state changes.
+          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+          // Handle the initial sign-in state.
+          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+          authorizeButton.onclick = handleAuthClick;
+          signoutButton.onclick = handleSignoutClick;
+        });
+}
+
+function updateSigninStatus(isSignedIn) {
+        if (isSignedIn) {
+          authorizeButton.style.display = 'none';
+          signoutButton.style.display = 'block';
+          listMajors();
+        } else {
+          authorizeButton.style.display = 'block';
+          signoutButton.style.display = 'none';
+        }
+      }
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+function appendPre(message) {
+        var pre = document.getElementById('content');
+        var textContent = document.createTextNode(message + '\n');
+        pre.appendChild(textContent);
+}
+
+function listMajors() {
+        gapi.client.sheets.spreadsheets.values.get({
+          spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+          range: 'Class Data!A2:E',
+        }).then(function(response) {
+          var range = response.result;
+          if (range.values.length > 0) {
+            appendPre('Name, Major:');
+            for (i = 0; i < range.values.length; i++) {
+              var row = range.values[i];
+              // Print columns A and E, which correspond to indices 0 and 4.
+              appendPre(row[0] + ', ' + row[4]);
+            }
+          } else {
+            appendPre('No data found.');
+          }
+        }, function(response) {
+          appendPre('Error: ' + response.result.error.message);
+        });
+}
